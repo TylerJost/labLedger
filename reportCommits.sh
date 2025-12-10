4c247e0f27218cd594e74b1540d81dc38fb1616d
Tyler Jost <tjost@bwh.harvard.edu>
2025-12-10 16:11:19 -0500

    adjust so that bash script ingests a config file instead

--- reportCommits.sh
+++ reportCommits.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-# Change these to your name/email
-AUTHOR="${1:-tjost@bwh.harvard.edu}"
-BASE_DIR="${2:-/data}"  # Change to the base directory containing your repos
-OUTPUT="${3:-/home/tyj566/misc/testEnv/gitLog.txt}"
-SUMMARIZE="${4:-true}"
+set -euo pipefail
+
+# Change this config to your name/email
+source labrat.conf
 
 # Clear old output
-> "$OUTPUT"
-
+# > "$OUTPUT"
+OUTPUTTEMP='./lastWeekOutput.txt'
+printf "\n\n`date +%F` ----\n" >> "$OUTPUT"
 # Find all .git folders and loop through their parent directories
 find "$BASE_DIR" -type d -name ".git" | while read gitdir; do
     REPO_DIR=$(dirname "$gitdir")
     	echo "$REPO_DIR"
-        echo "=== $REPO_DIR ===" >> "$OUTPUT"
+        echo "repository: $REPO_DIR" >> "$OUTPUT"
+
 	    (
 	            cd "$REPO_DIR" && \
 			            git log --since="1 week ago" --author="$AUTHOR" --format='%h %ad %s' --date=short >> "$OUTPUT"
-		        )
+		)
 			    echo "" >> "$OUTPUT"
+
 		    done
 
 		    echo "Done! Logs saved to $OUTPUT"
 
 if [[ "$SUMMARIZE" == 'true' ]]; then
 	echo "Generating summary"
-	pixi run python translate.py --text $OUTPUT
+	pixi run python translate.py --text $OUTPUT --d "`date +%F`"
 fi
