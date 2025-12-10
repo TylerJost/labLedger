18d5de823a002e98323aba2c420d8eb3ba113dd0
Tyler Jost <tjost@bwh.harvard.edu>
2025-12-10 16:11:42 -0500

    summarization script looks for date and only summarizes past that date

--- translate.py
+++ translate.py
@@ -1,3 +1,4 @@
+
 from transformers import pipeline
 import argparse
 
@@ -6,23 +7,34 @@ if __name__ == '__main__':
     parser.add_argument('-t','--text', 
                         help='Path to text file to be summarized', 
                         default='/home/tyj566/misc/gitLog.txt')
+    parser.add_argument('-d','--datestring', default='')
     args = vars(parser.parse_args())
     
     with open(args['text'], 'r') as f:
         text = f.read()
 
+    date = args['datestring']
+    print(f'{date=}')
     toSummarize = []
+    summarize = 0
     for line in text.splitlines():
-        if line.startswith("==="):
+        if line.startswith(date):
+            summarize = 1
+            print('Set summarizing')
+        if line.startswith("repository: "):
             continue
         elif line == "":
             continue
-        else:
+        elif summarize:
             lineParts = line.split(" ")
             lineText = " ".join(lineParts[2:])
             toSummarize.append(lineText)
+    print(toSummarize)
+    if len(toSummarize) == 0:
+        exit('Nothing to summarize, exiting')
+
     text = ". ".join(toSummarize)
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
     summary = summarizer(text, max_length=500, min_length=30, do_sample=False)[0]['summary_text']
     with open(args['text'], 'a') as f:
-        f.write(f"\n=== SUMMARY ===\n{summary}\n")
+        f.write(f"\nSummary:\n{summary}\n")
